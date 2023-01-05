8d17336ed0c0944ed9aa3e28fb81bbc45ee80ee8
Enrique Altamirano <enrique.altamiros@gmail.com>
2023-01-05 14:59:44 -0300

    agregando ejercicios

---
+++ Ejercicio2/ejercicio2.js
@@ -0,0 +1,10 @@
+let nombre = "Enrique Altamirano"
+let edad = 36
+let desarrollador = true
+let fechaNacimiento = new Date("05 06 1986")
+let libro = {
+    nombre: "Blitzscaling",
+    autor: "Hoffman, Reid",
+    nroPaginas: 320,
+    publishYear: 2021
+}
