68943f5cfe6b953769956bacca5ff054cdd77362
Enrique Altamirano <enrique.altamiros@gmail.com>
2023-01-05 21:23:48 -0300

    ejercicios modulo 5

---
+++ Modulo5/ejer1.js
@@ -0,0 +1,13 @@
+let numeroUno = 3.4
+let numeroDos = 4.9
+let sumaNumeros = numeroUno + numeroDos
+let decimal = 8.33333121212
+
+console.log( numeroUno + numeroDos )
+
+console.log( numeroUno - numeroDos )
+
+console.log(typeof sumaNumeros.toString())
+
+console.log(decimal)
+console.log(decimal.toFixed(2))
---
+++ Modulo5/ejercicios/ejercicio.js
@@ -0,0 +1,5 @@
+let alturaEntero = 173
+let alturaFlotante = 1.73
+let pesoFlotante = 96.3
+let pesoRoundUp = Math.round(pesoFlotante)
+let pesoRoundDown = Math.ceil(pesoFlotante)
